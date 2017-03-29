5cf7d225a93cca3bba54706a1a1f302d9919c5b3
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2017-03-29 20:56:20 +0300

    add 1_10

---
+++ 1_10.scala
@@ -0,0 +1,12 @@
+#!/usr/bin/env scala
+
+val string = "abcde"
+
+println("string: " + string)
+
+println("take(2): " + string.take(2))
+println("drop(3): " + string.drop(3))
+println("takeRight(2): " + string.takeRight(2))
+println("dropRight(3): " + string.dropRight(3))
+println("substring(2, 4): " + string.substring(2, 4))
+
